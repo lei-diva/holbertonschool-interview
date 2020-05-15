8f49e12071a20ebc82d74e6ca09d05bd7a91c93f
lei-diva <divaaleii@gmail.com>
2020-05-15 05:21:10 -0700

    binary tree node

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -8,6 +8,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
     binary_tree_t *node;
 
     node = malloc(sizeof(binary_tree_t));
+
+    if (!node) {
+        return(NULL);
+    }
     node->n = value;
     node->parent = parent;
     node->right = NULL;
