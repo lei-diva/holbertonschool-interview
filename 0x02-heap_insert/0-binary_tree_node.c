68ce40ff01b76d91edf37ff03cca17843bc73e26
lei-diva <divaaleii@gmail.com>
2020-05-15 05:20:45 -0700

    binary tree node

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+
+
+    binary_tree_t *node;
+
+    node = malloc(sizeof(binary_tree_t));
+    node->n = value;
+    node->parent = parent;
+    node->right = NULL;
+    node->left = NULL;
+
+    if (!parent)
+    {
+        parent = node;
+    }
+    else if (parent->n >= node->n) {
+        parent->left = node;
+    }
+    else {
+        parent->right = node;
+    }
+
+    return(node);
+}
---
+++ 0x02-heap_insert/README.md
@@ -0,0 +1,18 @@
+# 0x02. Heap Insert
+
+## Description
+What you should learn from this project:
+
+---
+
+### [0. New node](./0-binary_tree_node.c)
+* Write a function that creates a binary tree node:
+
+
+### [1. Heap - Insert](./1-heap_insert.c)
+* Write a function that inserts a value into a Max Binary Heap:
+
+---
+
+## Author
+* **Diva Lei** - [lei-diva](https://github.com/lei-diva)
