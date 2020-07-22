d388c3ebae62be5c2babcbecc46b8098c81e6eda
lei-diva <divaaleii@gmail.com>
2020-07-22 00:01:54 -0700

    lock

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -10,16 +10,7 @@ def canUnlockAll(boxes):
     while len(checker):
         check_box = checker.pop(0)
         for box in boxes[check_box]:
-            if isinstance(box, int) and box >= 0 and box < len(boxes) and not is_locked[box]:
+            if box >= 0 and box < len(boxes) and not is_locked[box]:
                 is_locked[box] = True
                 checker.append(box)
     return all(is_locked)
-
-
-
-
-
-
-
-
-
