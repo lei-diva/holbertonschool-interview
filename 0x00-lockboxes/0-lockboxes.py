87d6657e3af2927b8af614fd6ed0a19a89a984b6
lei-diva <divaaleii@gmail.com>
2020-07-21 23:59:31 -0700

    lockbox

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,12 +2,23 @@
 
 
 def canUnlockAll(boxes):
+    """ figures out if all boxes can be opened """
     is_locked = [False for i in range(len(boxes))]
     is_locked[0] = True
-
-    for i, box in enumerate(boxes):
-        for key in box:
-            if key != i:
-                is_locked[key] = True
-
+    checker = [0]
+    while len(checker):
+        check_box = checker.pop(0)
+        for box in boxes[check_box]:
+            if isinstance(box, int) and box >= 0 and box < len(boxes) and not is_locked[box]:
+                is_locked[box] = True
+                checker.append(box)
     return all(is_locked)
+
+
+
+
+
+
+
+
+
